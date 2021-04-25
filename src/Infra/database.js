6df81f3c49e9fadb3fd035652e6a94c4b30c9c05
gabrielle <gabriellebruna2011@hotmail.com>
2021-04-25 17:44:17 -0300

    update

--- src/Infra/database.js
+++ src/Infra/database.js
@@ -1,8 +1,8 @@
-require('dotenv').config({ path: './src/config/.env' });
-const mongoose = require("mongoose");
+require('dotenv').config({path: './src/Config/.env'});
+
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    console.log(process.env.DB_CONNECTION);
     try {
         await mongoose.connect(process.env.DB_CONNECTION, {
             useNewUrlParser: true,
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require("express");
+const cors = require('cors');
+
+const routes = require('./Routes/pacienteRota');
+const routes2 = require ('./Routes/vacinaRota');
+const Authroutes = require("./Routes/authRota");
+const connectDB = require("./Infra/database");
+
+connectDB();
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use('/auth', Authroutes );
+app.use('/paciente', routes);
+app.use('/vacina', routes2);
 
-
-app.post('/paciente', (request,response)=>{
-    console.log(request.body);
-    return response.json({"mensagem":`Olá ${request.body.nome}`});
+module.exports = app.listen(process.env.PORT || 3333, () => {
+    console.log("Server running");
 });
-
-app.get('/paciente/:nome', (request,response) =>{
-    return response.send("Olá mundo" + request.params.nome);
-
-});
-
-app.listen(3333);
