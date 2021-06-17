cb6f92b584c8fb276d871ec65b82b8df5c8bbe4e
gabrielle <gabriellebruna2011@hotmail.com>
2021-06-17 20:26:38 -0300

    Renomeando pastas

---
+++ src/data/pacienteRepositorio.js
@@ -0,0 +1,57 @@
+const pacienteRepositorio = require('../models/Paciente.js');
+
+//ok
+module.exports.buscaPacientePorEmail = async function (email) {
+    return pacienteRepositorio.findOne({ email });
+}
+
+//ok
+module.exports.insereUsuario = async function (novoUsuario) {
+    const { nome, email, senha } = novoUsuario;
+    const retornoUsuario = await pacienteRepositorio.create({
+        nome, email, senha
+    });
+    return retornoUsuario;
+}
+
+
+//#region Antigo
+module.exports.buscaPaciente = async function () {
+    return pacienteRepositorio.find();
+}
+
+module.exports.buscaPacientePorCpf = async function (cpf) {
+    return await pacienteRepositorio.find({ cpf });
+}
+
+module.exports.inserePaciente = async function (novoPaciente) {
+    const { nome, cpf, altura, peso, imc, classificacao, dataNascimento, cidade, UF, listaComorbidades, JaTeveCovid, email, senha } = novoPaciente;
+    const retornoPaciente = await pacienteRepositorio.create({
+        nome, cpf, altura, peso, imc, classificacao, dataNascimento, cidade, UF, listaComorbidades, JaTeveCovid, email, senha
+    });
+    return retornoPaciente;
+}
+
+module.exports.atualizaPaciente = async function (atualizaPaciente) {
+    const { nome, cpf, altura, peso, imc, classificacao, dataNascimento, cidade, UF, listaComorbidades, JaTeveCovid, email, senha } = atualizaPaciente;
+    const PacienteAtualizado = await pacienteRepositorio.updateOne(
+        { cpf, altura, peso, imc, classificacao, dataNascimento, cidade, UF, listaComorbidades, JaTeveCovid, email, senha },
+        {
+            $set:
+            {
+                nome
+            }
+        }
+    );
+    return PacienteAtualizado;
+}
+
+module.exports.removePaciente = async function (cpf) {
+    return pacienteRepositorio.deleteOne({ cpf });
+}
+
+module.exports.verificaEmailSenha = function (email, senha) {
+    return pacienteRepositorio.findOne({ email, senha });
+}
+
+//#endregion Antigo
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require('cors');
+
+const routes = require('./Routes/pacienteRota');
+const routes2 = require('./Routes/vacinaRota');
+const Authroutes = require("./Routes/authRota");
+const connectDB = require("./Infra/database");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use('/auth', Authroutes);
+app.use('/paciente', routes);
+app.use('/vacina', routes2);
+
+
+module.exports = app.listen(process.env.PORT || 3333, () => {
+    console.log("Server running");
+});
